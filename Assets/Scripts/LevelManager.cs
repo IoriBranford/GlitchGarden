112d89f304dba223256b4414f2a5a63314dd953e
IoriBranford <ioribranford@gmail.com>
2017-03-06 14:46:03 -0800

    The beginning, with splash and title screens scaling properly to the game resolution.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+	void Awake () {
+		SceneManager.sceneLoaded += SceneLoaded;
+	}
+
+	public void LoadLevel (string name) {
+		Debug.Log("Loading level " + name);
+		SceneManager.LoadScene(name);
+		StartLevel();
+	}
+
+	public void LoadNextLevel () {
+		int i = SceneManager.GetActiveScene().buildIndex + 1;
+		Debug.Log("Loading level " + i);
+		SceneManager.LoadScene(i);
+		StartLevel();
+	}
+
+	public void Quit () {
+		Debug.Log("Quitting");
+		Application.Quit();
+	}
+
+	void SceneLoaded (Scene scene, LoadSceneMode mode) {
+		switch (scene.name) {
+		case "00_Splash":
+			Invoke("LoadNextLevel", 3);
+			break;
+		}
+	}
+
+	void StartLevel () {
+		//Brick.LevelStarted();
+		//Gem.LevelStarted();
+	}
+
+	public void CheckNextLevel () {
+		//if (Brick.NumBreakables == 0 && Gem.NumGems == 0) {
+		//	LoadNextLevel();
+		//}
+	}
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+	static MusicPlayer _instance;
+
+	public AudioClip startMusic;
+	public AudioClip stageMusic;
+
+	private AudioSource _music;
+
+	void Awake () {
+		if (_instance == null) {
+			GameObject.DontDestroyOnLoad(gameObject);
+			_instance = this;
+			SceneManager.sceneLoaded += OnLoadedScene;
+			_music = GetComponent<AudioSource>();
+		} else if (_instance != this) {
+			Destroy(gameObject);
+		}
+	}
+
+	// Between Awake and Start is when an audio source starts playing
+
+	void OnLoadedScene (Scene scene, LoadSceneMode mode) {
+		_music.Stop();
+		switch (scene.name) {
+		case "Start":
+			_music.clip = startMusic;
+			break;
+		case "Stage":
+			_music.clip = stageMusic;
+			break;
+		default:
+			return;
+		}
+		_music.loop = true;
+		_music.Play();
+	}
+}
