9e15637eee09ddea9bc77f6caa2bc248f9a8c9bc
IoriBranford <ioribranford@gmail.com>
2017-03-07 11:28:19 -0800

    Title screen fade in.

---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour {
+	public bool fadeIn = true;
+	public float fadeTime = 1;
+
+	private Image _image;
+	private float _fadeDirection;
+
+	// Use this for initialization
+	void Start () {
+		_image = GetComponent<Image>();
+
+		Color color = _image.color;
+		if (fadeIn) {
+			color.a = 1;
+		} else {
+			color.a = 0;
+		}
+		_image.color = color;
+		_fadeDirection = fadeIn ? -1 : 1;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Time.timeSinceLevelLoad < fadeTime) {
+			Color color = _image.color;
+			color.a += _fadeDirection * Time.deltaTime / fadeTime;
+			_image.color = color;
+		} else {
+			gameObject.SetActive(false);
+		}
+	}
+}
