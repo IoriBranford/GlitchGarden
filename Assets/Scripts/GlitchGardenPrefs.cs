324c16f6fbd03e1abd1c33b4edfb36cc1c7b8311
IoriBranford <ioribranford@gmail.com>
2017-04-03 17:31:22 -0700

    Create prefs wrapper.

---
+++ Assets/Scripts/GlitchGardenPrefs.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GlitchGardenPrefs : MonoBehaviour {
+	const string MasterVolumeKey = "MasterVolume";
+	public static float MasterVolume {
+		get {
+			return PlayerPrefs.GetFloat(MasterVolumeKey);
+		}
+
+		set {
+			if (value < 0f || value > 1f) {
+				Debug.LogError(MasterVolumeKey +
+						" must be between 0 and 1");
+			} else {
+				PlayerPrefs.SetFloat(MasterVolumeKey, value);
+			}
+		}
+	}
+
+	const string DifficultyKey = "Difficulty";
+	public static float Difficulty {
+		get {
+			return PlayerPrefs.GetFloat(DifficultyKey);
+		}
+
+		set {
+			if (value < 0f || value > 1f) {
+				Debug.LogError(DifficultyKey +
+						" must be between 0 and 1");
+			} else {
+				PlayerPrefs.SetFloat(DifficultyKey, value);
+			}
+		}
+	}
+
+	const string UnlockedLevelKey = "UnlockedLevel";
+	public static void UnlockLevel (int level) {
+		if (level < 0 || level >= SceneManager.sceneCountInBuildSettings) {
+			Debug.LogError(UnlockedLevelKey +
+					" must be between 0 and " +
+					SceneManager.sceneCountInBuildSettings);
+		} else {
+			PlayerPrefs.SetInt(UnlockedLevelKey + level, 1);
+		}
+	}
+	public static bool IsLevelUnlocked (int level) {
+		if (level < 0 || level >= SceneManager.sceneCountInBuildSettings) {
+			Debug.LogError(UnlockedLevelKey +
+					" must be between 0 and " +
+					SceneManager.sceneCountInBuildSettings);
+			return false;
+		}
+
+		return PlayerPrefs.GetInt(UnlockedLevelKey + level) == 1;
+	}
+}
